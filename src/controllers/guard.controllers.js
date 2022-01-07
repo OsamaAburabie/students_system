3e51464391bb294b2bb4511f9c9a48ee4853f573
Osama Aburbie <aburabie.osama@gmail.com>
2022-01-07 08:54:40 +0200

    before cloudinary

--- src/controllers/guard.controllers.js
+++ src/controllers/guard.controllers.js
@@ -4,9 +4,17 @@ const mongoose = require("mongoose");
 
 const getStudent = async (req, res) => {
   isValid = mongoose.Types.ObjectId.isValid(req.params.id);
-  if (!isValid) return res.status(400).json({ message: "Invalid ID" });
+  if (!isValid)
+    return res.status(400).json({
+      success: false,
+      message: "Invalid ID",
+    });
   let user = await User.findOne({ _id: req.params.id });
-  if (!user) return res.status(404).send("User not found");
+  if (!user)
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
   // delete user.password;
   user = user.toObject();
   delete user.password;
@@ -14,7 +22,11 @@ const getStudent = async (req, res) => {
   //======================= Get the vaccine ============================
   let vaccine = await Vaccine.findOne({ student_id: req.params.id });
 
-  res.status(200).json({ user, vaccine });
+  res.status(200).json({
+    success: true,
+    user,
+    vaccine,
+  });
 };
 
 module.exports = {
